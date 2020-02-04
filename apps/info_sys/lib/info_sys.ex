c7b77de3eafe3bfe7f2452f61c4edce5d9f37162
Krzysztof Białek <krzysztof.bialek@gmail.com>
2020-02-04 21:47:46 +0100

    add basic InfoSys

--- apps/info_sys/lib/info_sys.ex
+++ apps/info_sys/lib/info_sys.ex
@@ -12,7 +12,25 @@ defmodule InfoSys do
       :world
 
   """
-  def hello do
-    :world
+  @backends [InfoSys.Wolfram]
+
+  defmodule Result do
+    defstruct score: 0, text: nil, url: nil
+  end
+
+  def compute(query, opts \\ []) do
+    opts = Keyword.put_new(opts, :limit, 10)
+
+    backends = opts[:backends] || @backends
+
+    backends
+    |> Enum.map(&async_query(&1, query, opts))
+  end
+
+  defp async_query(backend, query, opts) do
+    Task.Supervisor.async_nolink(
+      InfoSys.TaskSupervisor, backend, :compute, 
+      [query, opts], shutdown: :brutal_kill
+    )
   end
 end
